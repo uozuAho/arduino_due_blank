c9224b1d9d931501d8385c839e641322fc85c94b
Warwick Stone <uozu.aho@gmail.com>
2013-07-14 22:22:36 +1000

    Added timeout to windows flash upload batch file  *  Attempting to flash immediately after resetting the arduino would result in the serial port not being found

--- upload.bat
+++ upload.bat
@@ -18,5 +18,7 @@ rem First force a reset & flash erase by opening and closing
 rem the serial port at 1200bps
 mode %PORT%:1200,n,8,1
 
+timeout /T 2
+
 rem Now use bossa to upload the binary file
 %BOSSA_LOC%\bossac.exe --port=%PORT% -U false -e -w -v -b %BINARY% -R
