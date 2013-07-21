2693e254723f61943969997a0a81c5f2d77dc7af
Warwick Stone <uozu.aho@gmail.com>
2013-07-21 10:09:32 +1000

    Added host unit test build configuration  *  Unit tests can now be run on the host using this config. May not work on linux, sorry! Maybe move to cmake instead of Eclipse...

--- test_harness/Unity/unity_conf.h
+++ test_harness/Unity/unity_conf.h
@@ -1,6 +1,10 @@
 #ifndef UNITY_CONF_H
 #define UNITY_CONF_H
 
-#define UNITY_OUTPUT_CHAR(a)      ArduinoPutchar(a)
+#ifdef RUN_UNIT_TESTS_HOST
+    #define UNITY_OUTPUT_CHAR(a)      putchar(a)
+#else
+    #define UNITY_OUTPUT_CHAR(a)      ArduinoPutchar(a)
+#endif
 
 #endif
