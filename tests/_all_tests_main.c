b5fcf1a281489ffcbc025e38c42ec3c44cf05378
Warwick Stone <uozu.aho@gmail.com>
2013-08-02 11:06:12 +1000

    Added host unit test Eclipse build variant

--- tests/_all_tests_main.c
+++ tests/_all_tests_main.c
@@ -5,6 +5,11 @@ void RunAllTests(void);
 
 int main(int argc, char* argv[])
 {
+    #ifdef UNIT_TEST_VARIANT1
+        MyStaticLib_vPrintTestMessage();
+        MyStaticLib_vPrintTestMessage();
+        MyStaticLib_vPrintTestMessage();
+    #endif
     MyStaticLib_vPrintTestMessage();
 
     return UnityMain(argc, argv, RunAllTests);
